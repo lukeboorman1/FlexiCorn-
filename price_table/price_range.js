dccb9df797df624267f842517d43a751bf9964b2
Stef Kors <stef.kors@gmail.com>
2017-06-20 21:17:14 +0200

    number updates

--- price_table/price_range.js
+++ price_table/price_range.js
@@ -1,13 +1,22 @@
+$(document).ready(function() {
+	july_min = 11.79705512;
+	july_max = 12.0318189;
 
+	var total = setInterval(function() {
+		july = randomFloatBetween(july_min, july_max, 8);
+		$("#july_bushel").html(july);
+		$("#july_kg").html(july / 1000);
+		$("#july_gr").html(july / 1000000);
+		$("#july_mg").html(july / 1000000000);
 
-setInterval(function(){
+	}, 100);
 
-function getRandomInt(min,max) {
-  min = Math.ceil(11.79705512);
-  max = Math.floor(12.0318189);
-  return Math.floor(Math.random() * (max - mix)) + min; //The maximum is exclusive and the minimum is inclusive
-};
+	function randomFloatBetween(minValue, maxValue, precision) {
+		if (typeof(precision) == 'undefined') {
+			precision = 2;
+		}
+		minValue = minValue - 1;
+		return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)), maxValue).toFixed(precision));
+	}
 
-},3000);
-Var total = getRandomInt()
-$("#getRandomInt").html(total);
+});
