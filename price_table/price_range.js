b93dd5eef2cacf493f8711d9f6f286064b4675e3
Victoria Douka-Doukopoulou <doukadoukadouka@Victorias-MacBook-Pro.local>
2017-06-24 19:26:46 +0200

    price_table decimal fix

--- price_table/price_range.js
+++ price_table/price_range.js
@@ -13,29 +13,33 @@ $(document).ready(function() {
 
 
 	var total = setInterval(function() {
-		july17 = randomFloatBetween(july17_min, july17_max, 8);
+		july17_raw = randomFloatBetween(july17_min, july17_max, 8);
+		july17 = july17_raw.toFixed(4);
 		$("#july17_bushel").html(july17);
-		$("#july17_kg").html(july17 / 1000);
-		$("#july17_gr").html(july17 / 1000000);
-		$("#july17_mg").html(july17 / 1000000000);
+		$("#july17_kg").html(july17 / 100);
+		$("#july17_gr").html(july17 / 10000);
+		$("#july17_mg").html(july17 / 100000);
 
-		september17 = randomFloatBetween(september17_min, september17_max, 8);
+		september17_raw = randomFloatBetween(september17_min, september17_max, 8);
+		september17 = september17_raw.toFixed(4);
 		$("#september17_bushel").html(september17);
-		$("#september17_kg").html(september17 / 1000);
-		$("#september17_gr").html(september17 / 1000000);
-		$("#september17_mg").html(september17 / 1000000000);
+		$("#september17_kg").html(september17 / 100);
+		$("#september17_gr").html(september17 / 1000);
+		$("#september17_mg").html(september17 / 100000);
 
-		december17 = randomFloatBetween(december17_min, december17_max, 8);
+		december17_raw = randomFloatBetween(december17_min, december17_max, 8);
+		december17 = december17_raw.toFixed(4);
 		$("#december17_bushel").html(december17);
-		$("#december17_kg").html(december17 / 1000);
-		$("#december17_gr").html(december17 / 1000000);
-		$("#december17_mg").html(december17 / 1000000000);
+		$("#december17_kg").html(december17 / 100);
+		$("#december17_gr").html(december17 / 1000);
+		$("#december17_mg").html(december17 / 100000);
 
-		march18 = randomFloatBetween(march18_min, march18_max, 8);
+		march18_raw = randomFloatBetween(march18_min, march18_max, 8);
+		march18 = march18_raw.toFixed(4);
 		$("#march18_bushel").html(march18);
-		$("#march18_kg").html(march18 / 1000);
-		$("#march18_gr").html(march18 / 1000000);
-		$("#march18_mg").html(march18 / 1000000000);
+		$("#march18_kg").html(march18 / 100);
+		$("#march18_gr").html(march18 / 1000);
+		$("#march18_mg").html(march18 / 100000);
 
 	}, 1000);
 
